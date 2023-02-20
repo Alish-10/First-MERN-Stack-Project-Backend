d08773b85c32a696a0a80ecccae42f38af8a31eb
Alish-10 <alishdahal77@gmail.com>
2023-02-20 21:42:16 +0545

    some refinements

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -5,25 +5,24 @@ const MIME_TYPE_MAP = {
   'image/png': 'png',
   'image/jpeg': 'jpeg',
   'image/jpg': 'jpg'
-}
+};
 
 const fileUpload = multer({
-  limits: 50000,
+  limits: 500000,
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'uploads/images')
-
+      cb(null, 'uploads/images');
     },
-    fileName: (req, file, cb) => {
-      const ext = MIME_TYPE_MAP[file.mimetype]
-      cd(null, uuid() + '.' + ext);
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuid() + '.' + ext);
     }
   }),
   fileFilter: (req, file, cb) => {
-    const isValid = !!MIME_TYPE_MAP[file.mimetype]
-    let error = isValid ? null : new Error('Invalid mime type!')
-    cb(error, isValid)
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error('Invalid mime type!');
+    cb(error, isValid);
   }
 });
 
-multer.exports = fileUpload;
+module.exports = fileUpload;
