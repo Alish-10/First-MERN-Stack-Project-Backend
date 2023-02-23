17cf8c095851e2d02906cae0396bf505019d9947
Alish-10 <alishdahal77@gmail.com>
2023-02-23 20:45:12 +0545

    backend wrapup

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -92,7 +92,7 @@ const createPlace = async (req, res, next) => {
     );
   }
 
-  const { title, description, address, creator } = req.body;
+  const { title, description, address } = req.body;
 
   let coordinates;
   try {
@@ -107,11 +107,11 @@ const createPlace = async (req, res, next) => {
     address,
     location: coordinates,
     image: req.file.path,
-    creator
+    creator: req.userData.userId
   });
   let user;
   try {
-    user = await User.findById(creator);
+    user = await User.findById(req.userData.userId);
   } catch (err) {
     const error = new HttpError('Creating place failed,please try again', 500);
     return next(error); S
